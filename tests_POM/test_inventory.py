b4d17ab59eb3f3532f77bd6e74b9db03cbc8a8c6
Nympwn <yesenin_socio@mail.ru>
2025-11-02 23:19:35 +0500

    big homework

--- tests_POM/test_inventory.py
+++ tests_POM/test_inventory.py
@@ -13,11 +13,39 @@ def test_add_product(driver):
     inventory_page.add_to_cart('Sauce Labs Backpack')
     inventory_page.add_to_cart('Sauce Labs Fleece Jacket')
 
+def test_get_cart_count(driver):
+
+    login_page = LoginPage(driver)
+    inventory_page = InventoryPage(driver)
+
+    login_page.open_login_page()
+    login_page.login('standard_user', 'secret_sauce')
+
+    inventory_page.add_to_cart('Sauce Labs Backpack')
+    inventory_page.add_to_cart('Sauce Labs Fleece Jacket')
+
     assert inventory_page.get_cart_count(driver) == 2
 
-    inventory_page.filter(2)
+def test_filter(driver):
+    login_page = LoginPage(driver)
+    inventory_page = InventoryPage(driver)
 
+    login_page.open_login_page()
+    login_page.login('standard_user', 'secret_sauce')
+
+    inventory_page.filter(2)
     assert inventory_page.filter(2).is_selected() == True
+
+def test_add_to_cart(driver):
+
+    login_page = LoginPage(driver)
+    inventory_page = InventoryPage(driver)
+
+    login_page.open_login_page()
+    login_page.login('standard_user', 'secret_sauce')
+
+    inventory_page.add_to_cart('Sauce Labs Backpack')
+    inventory_page.add_to_cart('Sauce Labs Fleece Jacket')
 
     inventory_page.go_to_cart()
 
